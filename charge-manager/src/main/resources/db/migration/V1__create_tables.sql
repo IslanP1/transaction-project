d6096da99f00d7589a560ac3f44df5af980d3129
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-12-20 22:23:11 -0300

    feat: Adicionar tabela de status de cobrança e atualizar a tabela de cobrança para o status de referência.

--- charge-manager/src/main/resources/db/migration/V1__create_tables.sql
+++ charge-manager/src/main/resources/db/migration/V1__create_tables.sql
@@ -8,6 +8,18 @@ CREATE TABLE tb_client (
     phone VARCHAR(20)
 );
 
+CREATE TABLE tb_charge_status (
+    id SERIAL PRIMARY KEY,
+    code VARCHAR(30) NOT NULL UNIQUE, -- PENDING, REGISTERED, PAID...
+    description VARCHAR(255) NOT NULL
+);
+
+INSERT INTO tb_charge_status (code, description) VALUES
+    ('PENDING', 'Cobrança pendente'),
+    ('REGISTERED', 'Cobrança registrada'),
+    ('PAID', 'Cobrança paga'),
+    ('CANCELED', 'Cobrança cancelada');
+
 -- Tabela de Cobranças (Charge)
 -- Status baseados no requisito [f4]: PENDING, REGISTERED, CANCELED, PAID
 CREATE TABLE tb_charge (
@@ -15,8 +27,9 @@ CREATE TABLE tb_charge (
     description VARCHAR(255),
     amount DECIMAL(10, 2) NOT NULL,
     due_date DATE NOT NULL,
-    status VARCHAR(20) NOT NULL DEFAULT 'PENDING',
+    status_id INTEGER NOT NULL SET DEFAULT 1, -- Referência para tb_charge_status [f4]
     payment_method VARCHAR(20), -- PIX, BOLETO, CARTAO_CREDITO [f3]
     client_id INTEGER NOT NULL,
-    CONSTRAINT fk_client FOREIGN KEY (client_id) REFERENCES tb_client(id)
+    CONSTRAINT fk_client FOREIGN KEY (client_id) REFERENCES tb_client(id) ON DELETE RESTRICT,
+    CONSTRAINT fk_charge_status FOREIGN KEY (status_id) REFERENCES tb_charge_status(id) ON DELETE RESTRICT
 );
