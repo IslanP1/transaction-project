973a8ef2990b49647e0fbd64ea4367fa8a543342
ThamiresAntunes <antunesthamires8@gmail.com>
2026-02-01 20:24:05 -0300

    feat(charge-proxy): adiciona OpenFeign e validação como dependências

--- charge-proxy/src/main/java/com/adsifpb/charge_proxy/ChargeProxyApplication.java
+++ charge-proxy/src/main/java/com/adsifpb/charge_proxy/ChargeProxyApplication.java
@@ -2,8 +2,10 @@ package com.adsifpb.charge_proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ChargeProxyApplication {
 
 	public static void main(String[] args) {
